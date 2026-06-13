db70b8babebced3bc913e1f6380c8e02e28202f2
Rohit <Rohitstha6165@gmail.com>
2026-06-13 22:57:45 +1000

    Add restaurants API

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,42 @@ const PORT = 5001;
 
 app.use(express.json());
 
+const restaurants = [
+  {
+    id: 1,
+    name: "Foodify Grill",
+    cuisine: "American",
+    rating: 4.6,
+    deliveryTime: "25-35 min",
+    isOpen: true,
+    hours: "10:00 AM - 10:00 PM",
+  },
+  {
+    id: 2,
+    name: "Pizza Palace",
+    cuisine: "Italian",
+    rating: 4.7,
+    deliveryTime: "30-40 min",
+    isOpen: true,
+    hours: "11:00 AM - 11:00 PM",
+  },
+  {
+    id: 3,
+    name: "Spice House",
+    cuisine: "Indian",
+    rating: 4.8,
+    deliveryTime: "35-45 min",
+    isOpen: false,
+    hours: "12:00 PM - 9:00 PM",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Foodify backend is running");
+});
+
+app.get("/api/restaurants", (req, res) => {
+  res.json(restaurants);
 });
 
 app.listen(PORT, () => {
