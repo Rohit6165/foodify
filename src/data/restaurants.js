b29ef071704b3b6d1e33df558e0c10f1dcdec002
Rohit <Rohitstha6165@gmail.com>
2026-06-13 15:09:53 +1000

    Add restaurant data

---
+++ src/data/restaurants.js
@@ -0,0 +1,25 @@
+const restaurants = [
+  {
+    id: 1,
+    name: "Foodify Grill",
+    cuisine: "American",
+    rating: 4.6,
+    deliveryTime: "25-35 min",
+  },
+  {
+    id: 2,
+    name: "Pizza Palace",
+    cuisine: "Italian",
+    rating: 4.7,
+    deliveryTime: "30-40 min",
+  },
+  {
+    id: 3,
+    name: "Spice House",
+    cuisine: "Indian",
+    rating: 4.8,
+    deliveryTime: "35-45 min",
+  },
+];
+
+export default restaurants;
