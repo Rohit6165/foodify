11408098d2cc7fea316095bbd163070e4f701394
Rohit <Rohitstha6165@gmail.com>
2026-06-14 01:22:02 +1000

    Connect backend to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
-
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 const PORT = 5001;
 
 app.use(cors());
-
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error.message);
+  });
 
 const restaurants = [
   {
