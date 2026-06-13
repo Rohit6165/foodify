5e12da91eed05039dfcc5753fa3ccd7d1dc2e719
Rohit <Rohitstha6165@gmail.com>
2026-06-13 15:08:15 +1000

    Add food data

---
+++ src/data/foods.js
@@ -0,0 +1,25 @@
+const foods = [
+  {
+    id: 1,
+    name: "Cheese Burger",
+    category: "Burger",
+    price: 8.99,
+    restaurant: "Foodify Grill",
+  },
+  {
+    id: 2,
+    name: "Pepperoni Pizza",
+    category: "Pizza",
+    price: 12.99,
+    restaurant: "Pizza Palace",
+  },
+  {
+    id: 3,
+    name: "Chicken Biryani",
+    category: "Rice",
+    price: 10.99,
+    restaurant: "Spice House",
+  },
+];
+
+export default foods;
