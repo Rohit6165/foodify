81fca46e985f2f2eed512f9e2ac4520f90ab9ae4
Rohit <Rohitstha6165@gmail.com>
2026-06-13 23:02:17 +1000

    Add foods API

--- backend/server.js
+++ backend/server.js
@@ -35,12 +35,46 @@ const restaurants = [
   },
 ];
 
+const foods = [
+  {
+    id: 1,
+    name: "Cheese Burger",
+    category: "Burger",
+    price: 8.99,
+    restaurant: "Foodify Grill",
+    badge: "Popular",
+    image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600",
+  },
+  {
+    id: 2,
+    name: "Pepperoni Pizza",
+    category: "Pizza",
+    price: 12.99,
+    restaurant: "Pizza Palace",
+    badge: "New",
+    image: "https://images.unsplash.com/photo-1628840042765-356cda07504e?w=600",
+  },
+  {
+    id: 3,
+    name: "Chicken Biryani",
+    category: "Rice",
+    price: 10.99,
+    restaurant: "Spice House",
+    badge: "Spicy",
+    image: "https://images.unsplash.com/photo-1589302168068-964664d93dc0?w=600",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Foodify backend is running");
 });
 
 app.get("/api/restaurants", (req, res) => {
   res.json(restaurants);
+});
+
+app.get("/api/foods", (req, res) => {
+  res.json(foods);
 });
 
 app.listen(PORT, () => {
