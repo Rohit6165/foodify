301cd0c045e85abcc31dc2b2002cd7c4a8ee1fb9
Rohit <Rohitstha6165@gmail.com>
2026-06-14 01:49:29 +1000

    Use environment port for backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const Order = require("./models/Order");
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
